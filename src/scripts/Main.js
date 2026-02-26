320d832f744a9180ddf9d7e7721ba69ca9f7e527
Marc-A. R. <marcantoine.roy03@gmail.com>
2026-02-26 08:37:19 -0500

    Java script Main.js construit
    juste la base

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,16 @@
-import Icons from "./utils/Icons.js";
+import Icons from './utils/Icons.js';
 
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    //console.log('test');
+
+    Icons.load();
+  }
+}
+
+new Main();
 // La ligne suivante devrait être au TOUT début du init() du Main
-Icons.load();
